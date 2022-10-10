24995fd3dbd194e25054876bc2145d3c2092bc06
sabouquier <sacha.bouquier@etu.uca.fr>
2022-10-10 23:06:29 +0200

    agedbrie delete

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -5,23 +5,13 @@ class GildedRose {
 
     public GildedRose(Item[] items) {
         this.items = items;
-    }
+    }    
 
     public void sulfuras(int i){
         return;
     }
 
-    public void agedBrie(int i){
-        item[i].sellIn--;
-        if(item[i].quality < 50){
-            item[i].quality++;
-        }
-        if(item[i].quality < 50 && item[i].sellIn < 0){
-            item[i].sellIn++;
-        }
-        return;
-
-    }
+   
 
     public void backstage(int i){
 
@@ -36,9 +26,13 @@ class GildedRose {
             if(items[i].name == "Sulfuras, Hand of Ragnaros"){
                 sulfuras(i);
             }
-            if(items[i].name == "Aged Brie"){
+           /* if(items[i].name == "Aged Brie"){
                 agedBrie(i);
-            }
+            }*/
+            /*if(items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                backstage(i);
+            }*/
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
