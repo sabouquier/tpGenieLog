a55a9a8cc436e3fe3b4bebfb8f312821cc50e432
sabouquier <sacha.bouquier@etu.uca.fr>
2022-10-11 17:22:51 +0200

    backstage pt 1

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -34,27 +34,11 @@ class GildedRose {
             
 
 
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
-
-                if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        } else {
-                            items[i].quality = 0;
-                        }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
+        items[i].sellIn--;
+        if(items[i].sellIn < 0 ){
+            items[i].quality = 0;
+        }
+        
     }
 
     public void other(int i){
