277c1990e65ecc0ef5944533322fb2488ec96bf9
sabouquier <sacha.bouquier@etu.uca.fr>
2022-10-11 17:28:34 +0200

    second test

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -42,26 +42,11 @@ class GildedRose {
         if (items[i].quality > 0) {
             items[i].quality--;
         }
-    
+                  
+        else {
+            if (items[i].quality < 50) {
+                        items[i].quality++;
 
-                    
-                 else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-                        }
                     }
                 }
 
