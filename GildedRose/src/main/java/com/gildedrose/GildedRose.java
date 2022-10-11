b9841fd5fd9264aa88702a5359b311c1695fa810
sabouquier <sacha.bouquier@etu.uca.fr>
2022-10-11 17:31:31 +0200

    other finished

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -45,32 +45,22 @@ class GildedRose {
                   
         else {
             if (items[i].quality < 50) {
-                        items[i].quality++;
+                items[i].quality++;
+            }
+        }
 
-                    }
-                }
+        items[i].sellIn = items[i].sellIn - 1;
+                
 
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
-
-                if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        } else {
-                            items[i].quality = 0;
-                        }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
+        if (items[i].sellIn < 0) {
+            if (items[i].quality > 0) {
+                items[i].quality = items[i].quality - 1;
+            }
+        } else {
+            items[i].quality = 0;
+        }
+                
+        
     }
 
     public void updateQuality() {
